c5fcfd24ca10b10d1c5c10fb29a9cdf6889d3378
Vishal Jain <vjain14@ford.com>
2023-01-24 19:15:37 +0530

    24th Jan

--- src/Day1.java
+++
@@ -1,5 +0,0 @@
-public class Day1 {
-    public static void main(String[] args){
-        System.out.println("Lets do 30 days of code challenge");
-    }
-}
---
+++ src/Everyday_Logs.java
@@ -0,0 +1,7 @@
+public class Everyday_Logs {
+    public static void main(String[] args){
+        System.out.println("Lets do 30 days of code challenge");
+        System.out.println("On 23rd Jan, I Revised SQL and practiced on HackerRank");
+        System.out.println("On 24th Jan, I practiced SQL on HackerRank");
+    }
+}

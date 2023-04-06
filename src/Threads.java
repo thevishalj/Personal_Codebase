c35a1c02938751e5c308c79a56d93a6905fa85e0
Vishal Jain <vjain14@ford.com>
2023-04-06 16:26:41 +0530

    Reduced Threads.java using lambada

--- src/Threads.java
+++ src/Threads.java
@@ -1,33 +1,19 @@
-class A implements Runnable {
-    public void run (){
-        for(int i=0; i<=100; i++){
-            System.out.println("Hi");
-            try {
-                Thread.sleep(15);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
-class B implements Runnable {
-    public void run (){
-        for(int i=0; i<=100; i++){
-            System.out.println("Hello");
-            try {
-                Thread.sleep(15);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
 public class Threads {
     public static void main(String[] args){
-        A obj1 = new A();
-        B obj2 = new B();
-
+        Runnable obj1 = () ->                       // we can only implement lamda on functional Interfaces
+        {
+            for(int i=0; i<=10; i++){
+                System.out.println("Hi");
+                try {Thread.sleep(15);} catch (InterruptedException e) {throw new RuntimeException(e);}
+            }
+        };
+        Runnable obj2 = () ->
+        {
+            for(int i=0; i<=10; i++){
+                System.out.println("Hello");
+                try {Thread.sleep(15);} catch (InterruptedException e) {throw new RuntimeException(e);}
+            }
+        };
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
 
